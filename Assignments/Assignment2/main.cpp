38fe216966e06001a8dca16ddec0cb01188ad5d9
JZJ <534290047@qq.com>
2021-05-16 16:16:39 +0800

    cmake assign2

--- Assignments/Assignment2/main.cpp
+++ Assignments/Assignment2/main.cpp
@@ -25,14 +25,46 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
 Eigen::Matrix4f get_model_matrix(float rotation_angle)
 {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f rotate = Eigen::Matrix4f::Identity();
+    float rotation_radian = MY_PI * rotation_angle / 180.0f;
+    rotate << cos(rotation_radian), -sin(rotation_radian), 0, 0,
+        sin(rotation_radian), cos(rotation_radian), 0, 0, 
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+
+    model = rotate * model;
     return model;
 }
 
 Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio, float zNear, float zFar)
 {
-    // TODO: Copy-paste your implementation from the previous assignment.
-    Eigen::Matrix4f projection;
+    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f Ortho = Eigen::Matrix4f::Identity();
 
+    float eye_fov_radian = MY_PI * eye_fov / 180.0f;
+    float t = tan(eye_fov_radian/2) * zNear;
+    float r = aspect_ratio * t;
+    Eigen::Matrix4f Ortho_scale = Eigen::Matrix4f::Identity();
+    Ortho_scale << 1/r, 0, 0, 0,
+    0, 1/t, 0, 0,
+    0, 0, 2/abs(zNear-zFar),
+    0, 0, 0, 0, 1;
+    Eigen::Matrix4f Ortho_trans = Eigen::Matrix4f::Identity();
+    Ortho_trans << 1, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 1, -(zNear+zFar)/2,
+    0, 0, 0, 1;
+
+    Ortho = Ortho_scale * Ortho_trans;
+    
+    Eigen::Matrix4f Persp2Ortho = Eigen::Matrix4f::Identity();
+    Persp2Ortho << zNear, 0, 0, 0,
+    0, zNear, 0, 0,
+    0, 0, zNear+zFar, -zNear*zFar,
+    0, 0, 1, 0;
+
+    projection = Ortho * Persp2Ortho;
+    
     return projection;
 }
 
